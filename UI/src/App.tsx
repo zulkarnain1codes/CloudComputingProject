cba97c3a747653977593f379fd37975aa1b07575
Ritik Kadwane <s4068947@student.rmit.edu.au>
2026-04-19 20:46:51 +1000

    Add register page in React MUI TypeScript and fix gitignore

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MainPage from "./pages/MainPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/main" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
